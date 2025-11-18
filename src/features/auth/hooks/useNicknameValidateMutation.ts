021cdb26dfbc18f5cfeae89dcc387168b8c6d846
yeeun-kor <jovm268@naver.com>
2025-11-18 04:55:05 +0000

    fix : deploy 브랜치명 수정

--- src/features/auth/hooks/useNicknameValidateMutation.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-import { validateNickname } from '../api/auth';
-
-export const useNicknameValidateMutation = () => {
-  return useMutation({
-    mutationFn: validateNickname,
-  });
-};
